ab67878b98b9346ce57baceaa1c4e0d0287efc93
WillisBai <willis.bai@outlook.com>
2025-02-06 09:35:18 +0800

    code improvements

--- slots/Cup2.py
+++ slots/Cup2.py
@@ -3,26 +3,23 @@
 
 """定义一个正常的Cup类，它没有任何属性"""
 class Cup(object):
-    __slots__ = ("name", "age") # 用tuple定义允许绑定的属性名称
+    __slots__ = ("name", "age") # 定义允许绑定的属性名称
     pass
 
 """*****开始测试*****"""
-cup = Cup()
-cup.name = "I'm a Cup" # 动态给实例绑定一个属性
-print(cup.name) # 正常case，打印结果：I'm a Cup
+cup_shadow = Cup()
+cup_shadow.name = "I'm a Cup" # 动态给实例绑定一个属性
+print(cup_shadow.name) # 打印结果：I'm a Cup
 
-def set_cup_desc(self, desc): # 定义一个方法
-    cup.desc = desc
+cup_shadow.age = 36
+print(cup_shadow.age)
 
-from types import MethodType
-cup.set_desc = MethodType(set_cup_desc,cup) # 动态给给实例绑定这个方法
-cup.set_desc("I'm the description for Cup.") #调用该方法
-print(cup.desc) # 正常case，打印结果：I'm the description for Cup.
+cup_shadow.desc = "this is a desc for cup shadow" # 不允许添加，会报错
+print(cup_shadow.desc) # 报错: AttributeError: 'Cup' object has no attribute 'desc'
 
-"""注意：给一个实例绑定的方法，对另一个实例是不起作用的，如果所有实例都需要该方法，才此方法直接定义在类中即可"""
 
-cup2 = Cup()
-cup2.set_desc("cup2 description") # 报错
+"""__slots__定义的属性仅对当前类实例起作用,对继承的子类是不起作用的,除非在子类中也定义__slots__"""
+
 
 
 
