f4d1f78ad1008bc14f8da6ad5f22643e22f812db
WillisBai <willis.bai@outlook.com>
2025-02-07 11:37:51 +0800

    code improvement

---
+++ basic/01_PrintSth.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 1 常规打印
+print("hello world")
+# 2 f-string方式打印
+name = "Alice"  # 字符串
+age = 30        # 整数
+height = 1.75   # 浮点数
+is_student = False # 布尔值
+print(f"Name: {name}, Age: {age}, Height: {height}, Is student: {is_student}") # py v3.6开始支持，v3.6以下版本可安装future-fstrings库实现兼容
