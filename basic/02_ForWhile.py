e436563f421cc01851a655bccf357ba8fad31f69
WillisBai <willis.bai@outlook.com>
2025-02-08 14:56:36 +0800

    ongoing implementation

---
+++ basic/02_ForWhile.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# for循环（第一种），从0到9，打印10个数，类似于java中的 for(i=0;i<10;i++)
+
+for i in range(10):
+    print(i)
+
+
+# for循环（第二种），遍历打印列表（java中称list）
+lists = ["apple", "banana", "cherry"]
+for item in lists:
+    print(item)
+
+
